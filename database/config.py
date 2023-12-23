572cf8aa7600646f8a87a733b8354491fd2a2b88
Nhamnnguyen258 <nhamnguyen258@gmail.com>
2023-12-23 23:56:23 +0700

    firt commit

---
+++ database/config.py
@@ -0,0 +1,26 @@
+import os
+from dotenv import load_dotenv
+from databases import Database
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Load environment variables from a .env file (if present)
+load_dotenv()
+
+# MySQL database URL
+database_url = os.getenv("DATABASE_URL", "mysql+aiomysql://root:Khongmatkhau1@localhost/app_ta")
+
+# Create async engine
+engine = create_async_engine(database_url, echo=True)
+
+# Create async session
+async_session = sessionmaker(
+    engine, expire_on_commit=False, autocommit=False, class_=AsyncSession
+)
+
+# Declarative Base
+Base = declarative_base()
+
+# Database instance
+database = Database(database_url)
